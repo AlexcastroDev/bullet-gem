da379a710aed55a67b6ee9e10ba5f5452c20304f
Alex Oliveira <alexoliveira7x@gmail.com>
2024-05-17 11:02:18 +0100

    more things

--- README.md
+++ README.md
@@ -2,27 +2,4 @@ Testing eager loading and Bullet gem
 
 # Logs
 
-```bash
-➜  bullet git:(main) ✗ ruby app.rb
-Run options: --seed 22888
-
-# Running:
-
-D, [2024-05-17T02:56:01.537037 #50948] DEBUG -- :   Order Load (0.1ms)  SELECT "orders".* FROM "orders" ORDER BY "orders"."id" ASC LIMIT ?  [["LIMIT", 1]]
-D, [2024-05-17T02:56:01.537843 #50948] DEBUG -- :   OrderItem Load (0.1ms)  SELECT "order_items".* FROM "order_items" WHERE "order_items"."order_id" = ?  [["order_id", 1]]
-D, [2024-05-17T02:56:01.539608 #50948] DEBUG -- :   TRANSACTION (0.0ms)  begin transaction
-D, [2024-05-17T02:56:01.539698 #50948] DEBUG -- :   OrderItem Update (0.1ms)  UPDATE "order_items" SET "name" = ?, "quantity" = ? WHERE "order_items"."id" = ?  [["name", "item d"], ["quantity", 6], ["id", 1]]
-D, [2024-05-17T02:56:01.539765 #50948] DEBUG -- :   TRANSACTION (0.0ms)  commit transaction
-item d
-D, [2024-05-17T02:56:01.539951 #50948] DEBUG -- :   TRANSACTION (0.0ms)  begin transaction
-D, [2024-05-17T02:56:01.539991 #50948] DEBUG -- :   OrderItem Update (0.0ms)  UPDATE "order_items" SET "name" = ?, "quantity" = ? WHERE "order_items"."id" = ?  [["name", "item g"], ["quantity", 4], ["id", 2]]
-D, [2024-05-17T02:56:01.540030 #50948] DEBUG -- :   TRANSACTION (0.0ms)  commit transaction
-item g
-D, [2024-05-17T02:56:01.540181 #50948] DEBUG -- :   TRANSACTION (0.0ms)  begin transaction
-D, [2024-05-17T02:56:01.540218 #50948] DEBUG -- :   OrderItem Update (0.0ms)  UPDATE "order_items" SET "name" = ?, "quantity" = ? WHERE "order_items"."id" = ?  [["name", "item c"], ["quantity", 7], ["id", 3]]
-D, [2024-05-17T02:56:01.540257 #50948] DEBUG -- :   TRANSACTION (0.0ms)  commit transaction
-item c
-.
-
-Finished in 0.013922s, 71.8288 runs/s, 0.0000 assertions/s.
-```
+log.txt
--- app.rb
+++ app.rb
@@ -45,14 +45,8 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
 end
 
-# ===== SEED =====
 
-# Create 3 items and 1 order
-order = Order.create!(name: 'o1', order_items: [
-  OrderItem.new(name: 'oi1', quantity: 1),
-  OrderItem.new(name: 'oi2', quantity: 2),
-  OrderItem.new(name: 'oi3', quantity: 3)
-])
+
 
 # ===== TEST =====
 ActiveRecord::Base.logger = Logger.new $stdout
@@ -60,6 +54,14 @@ require 'minitest/autorun'
 
 class ProblemTest < Minitest::Test
   def setup
+    # ===== SEED =====
+    # Create 3 items and 1 order
+    order = Order.create!(name: 'o1', order_items: [
+      OrderItem.new(name: 'oi1', quantity: 1),
+      OrderItem.new(name: 'oi2', quantity: 2),
+      OrderItem.new(name: 'oi3', quantity: 3)
+    ])
+
     Bullet.enable = true
     Bullet.rails_logger = true
     Bullet.raise = true
@@ -72,7 +74,24 @@ class ProblemTest < Minitest::Test
   def test_raise_n_plus_one_query
     order = Order.first
     order.order_items.each do |item|
-      item.update(name: "item #{('a'..'z').to_a.sample}", quantity: rand(1..10))
+      if item.quantity > 1
+        item.update(name: "item with color")
+      else 
+        item.update(name: "common item", quantity: rand(1..10))
+      end
+    end
+  end
+
+  def test_update_single_query
+    order = Order.includes(:order_items).first
+    OrderItem.transaction do
+      order.order_items.each do |item|
+        if item.quantity > 1
+          item.update!(name: "item with color")
+        else 
+          item.update!(name: "common item", quantity: rand(1..10))
+        end
+      end
     end
   end
 
@@ -81,5 +100,9 @@ class ProblemTest < Minitest::Test
     Bullet.end_request
     Bullet.enable = false
     puts "Bullet.enable = #{Bullet.enable?}"
+    
+    # delete order with all its items
+    Order.delete_all
+    OrderItem.delete_all
   end
 end
